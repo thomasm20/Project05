3fc1b7d51538a174c1ad0a48bfc288ee644c726a
thomasm20 <thomasm20@mail.wlu.edu>
2019-12-05 23:49:21 -0500

    updated

--- Complex.java
+++ Complex.java
@@ -33,17 +33,35 @@
 
 import java.util.Objects;
 
+/**
+ * A <code>Complex</code> class that .
+ *
+ * @author Joseph Salerno
+ * @author Brendan Olski
+ * @author Mitchell Thomas
+ *
+ * Class: Complex.java
+ * Project 5
+ */
+
 public class Complex {
     private final double re;   // the real part
     private final double im;   // the imaginary part
 
-    // create a new object with the given real and imaginary parts
+    /**
+     * Constructor method to create a new object with the given real and imaginary parts
+     *
+     */
     public Complex(double real, double imag) {
         re = real;
         im = imag;
     }
-
-    // return a string representation of the invoking Complex object
+    
+    /**
+     * toString method that returns a string representation of the invoking Complex object
+     * @return a string representation of the invoking Complex object
+     *
+     */
     public String toString() {
         if (im == 0) return re + "";
         if (re == 0) return im + "i";
@@ -51,25 +69,42 @@ public class Complex {
         return re + " + " + im + "i";
     }
 
+    /**
+     * Method that returns the absolute value of the invoking Complex object
+     * @return absolute value
+     *
+     */
     // return abs/modulus/magnitude
     public double abs() {
         return Math.hypot(re, im);
     }
 
-    // return angle/phase/argument, normalized to be between -pi and pi
+    /**
+     * Method that returns angle/phase/argument, normalized to be between -pi and pi
+     * @return angle/phase/argument
+     *
+     */
     public double phase() {
         return Math.atan2(im, re);
     }
 
-    // return a new Complex object whose value is (this + b)
+    /**
+     * Method that returns a new Complex object whose value is (this + b)
+     * @return a new Complex object whose value is (this + b)
+     *
+     */
     public Complex plus(Complex b) {
         Complex a = this;             // invoking object
         double real = a.re + b.re;
         double imag = a.im + b.im;
         return new Complex(real, imag);
     }
-
-    // return a new Complex object whose value is (this - b)
+    
+    /**
+     * Method that returns a new Complex object whose value is (this - b)
+     * @return a new Complex object whose value is (this - b)
+     *
+     */
     public Complex minus(Complex b) {
         Complex a = this;
         double real = a.re - b.re;
@@ -77,7 +112,11 @@ public class Complex {
         return new Complex(real, imag);
     }
 
-    // return a new Complex object whose value is (this * b)
+    /**
+     * Method that returns a new Complex object whose value is (this * b)
+     * @return a new Complex object whose value is (this * b)
+     *
+     */
     public Complex times(Complex b) {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
@@ -85,55 +124,101 @@ public class Complex {
         return new Complex(real, imag);
     }
 
-    // return a new object whose value is (this * alpha)
+    /**
+     * Method that returns a new object whose value is (this * alpha)
+     * @return a new object whose value is (this * alpha)
+     *
+     */
     public Complex scale(double alpha) {
         return new Complex(alpha * re, alpha * im);
     }
 
-    // return a new Complex object whose value is the conjugate of this
+    /**
+     * Method that returns a new Complex object whose value is the conjugate of this
+     * @return a new Complex object whose value is the conjugate of this
+     *
+     */
     public Complex conjugate() {
         return new Complex(re, -im);
     }
 
-    // return a new Complex object whose value is the reciprocal of this
+    /**
+     * Method that returns a new Complex object whose value is the reciprocal of this
+     * @return a new Complex object whose value is the reciprocal of this
+     *
+     */
     public Complex reciprocal() {
         double scale = re*re + im*im;
         return new Complex(re / scale, -im / scale);
     }
 
-    // return the real or imaginary part
+     /**
+     * Method that returns the real part
+     * @return the real part
+     *
+     */
     public double re() { return re; }
+    
+    /**
+     * Method that returns the imaginary part
+     * @return the imaginary part
+     *
+     */
     public double im() { return im; }
 
-    // return a / b
+    /**
+     * Method that returns a / b
+     * @return a / b
+     *
+     */
     public Complex divides(Complex b) {
         Complex a = this;
         return a.times(b.reciprocal());
     }
 
-    // return a new Complex object whose value is the complex exponential of this
+    /**
+     * Method that returns a new Complex object whose value is the complex exponential of this
+     * @return a new Complex object whose value is the complex exponential of this
+     *
+     */
     public Complex exp() {
         return new Complex(Math.exp(re) * Math.cos(im), Math.exp(re) * Math.sin(im));
     }
 
-    // return a new Complex object whose value is the complex sine of this
+    /**
+     * Method that returns a new Complex object whose value is the complex sine of this
+     * @return a new Complex object whose value is the complex sine of this
+     *
+     */
     public Complex sin() {
         return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
     }
 
-    // return a new Complex object whose value is the complex cosine of this
+    /**
+     * Method that returns a new Complex object whose value is the complex cosine of this
+     * @return a new Complex object whose value is the complex cosine of this
+     *
+     */
     public Complex cos() {
         return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
     }
 
-    // return a new Complex object whose value is the complex tangent of this
+    /**
+     * Method that returns a new Complex object whose value is the complex tangent of this
+     * @return a new Complex object whose value is the complex tangent of this
+     *
+     */
     public Complex tan() {
         return sin().divides(cos());
     }
     
 
 
-    // a static version of plus
+    /**
+     * A static version of plus
+     * @return sum
+     *
+     */
     public static Complex plus(Complex a, Complex b) {
         double real = a.re + b.re;
         double imag = a.im + b.im;
@@ -141,7 +226,11 @@ public class Complex {
         return sum;
     }
 
-    // See Section 3.3.
+    /**
+     * Method that returns 
+     * @return 
+     *
+     */
     public boolean equals(Object x) {
         if (x == null) return false;
         if (this.getClass() != x.getClass()) return false;
@@ -149,12 +238,19 @@ public class Complex {
         return (this.re == that.re) && (this.im == that.im);
     }
 
-    // See Section 3.3.
+    /**
+     * Method that returns 
+     * @return 
+     *
+     */
     public int hashCode() {
         return Objects.hash(re, im);
     }
 
-    // sample client for testing
+    /**
+     * Method that tests the sample client
+     *
+     */
     public static void main(String[] args) {
         Complex a = new Complex(5.0, 6.0);
         Complex b = new Complex(-3.0, 4.0);
